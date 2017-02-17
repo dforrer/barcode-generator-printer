dfe4605a275ce1f57b3ed15cf1696d82874cbcfa
Daniel <dforrer@gmail.com>
2017-02-17 19:54:39 +0100

    Initial Commit

---
+++ barcode-generator-printer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  barcode-generator-printer
+//
+//  Created by Daniel Forrer on 17.02.17.
+//  Copyright © 2017 Daniel Forrer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
