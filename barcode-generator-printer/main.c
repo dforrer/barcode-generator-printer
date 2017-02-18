5dbaba8859030244e5dccb73003fec391ee7aa33
Daniel <dforrer@gmail.com>
2017-02-18 04:54:41 +0100

    regex complete NEXT: write queue code

--- barcode-generator-printer/main.c
+++ barcode-generator-printer/main.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -6,6 +7,18 @@
 #include <regex.h>  
 
 /* reads in barcodes  */
+
+
+char* substring (const char* in, int from, int size) {
+    /* takes a const char array and returns a char* of size */
+    if (from+size <= strlen(in)) {
+        char *out = malloc(size+1);
+        sprintf(out, "%.*s", size ,&in[from]);
+        return out;
+    } else {
+        return NULL;
+    }
+}
 
 
 int write_year_to_file (int year) {
@@ -48,8 +61,8 @@ int read_year_from_file () {
 }
 
 
-void print_barcode (int year, char *filler, long belnr) {
-    printf("Barcode wird gedruckt: %i%s%ld\n", year, filler, belnr);
+void print_barcode (int year, long belnr) {
+    printf("Barcode wird gedruckt: %i%09ld\n", year, belnr);
     
     // TODO remove this
     return;
@@ -57,7 +70,7 @@ void print_barcode (int year, char *filler, long belnr) {
     // generate barcode
     
     char barcode_cmd[100];
-    sprintf(barcode_cmd, "barcode -b %i%s%ld -o out.ps -e \"i25\" -g \"590x300\";", year, filler, belnr);
+    sprintf(barcode_cmd, "barcode -b %i%09ld -o out.ps -e \"i25\" -g \"590x300\";", year, belnr);
     system(barcode_cmd);
     
     // convert .ps to .png with graphicsmagick
@@ -82,6 +95,7 @@ regex_t compile_regex (char * regex) {
     return r;
 }
 
+
 bool regex_matches (regex_t r, char * input) {
     
     int rv = regexec(&r, input, 0, NULL, 0);
@@ -97,19 +111,23 @@ bool regex_matches (regex_t r, char * input) {
     return false;
 }
 
+
 int main (void) {
     printf("Dieses Programm erlaubt das Drucken von Barcodes.\n");
     
-    int year = read_year_from_file();
+    int year  = read_year_from_file();
+    long belnr = 5000;
     printf("JAHR: %i\n", year);
     
-    char * x = (char*)malloc(10);
+    char * x = (char*)malloc(20);
     
     // Prepare regex
     regex_t regex_5000_9999             = compile_regex("^[5-9][0-9]{3}$");
     regex_t regex_100000_199999         = compile_regex("^1[0-9]{5}$");
     regex_t regex_range_5000_9999       = compile_regex("^[5-9][0-9]{3}-[5-9][0-9]{3}$");
     regex_t regex_range_100000_199999   = compile_regex("^1[0-9]{5}-1[0-9]{5}$");
+    regex_t regex_plus                  = compile_regex("^\\+$");
+    regex_t regex_year                  = compile_regex("^2[0-9]{3}$");
     
     
     while (true) {
@@ -117,49 +135,66 @@ int main (void) {
 
         /* Execute regular expression */
 
-        if ( regex_matches(regex_5000_9999, x) ) {
-            printf("5000er match!");
+        if ( regex_matches(regex_5000_9999, x) ||
+             regex_matches(regex_100000_199999, x) ) {
+            printf("5000/100000 match!\n");
             char *end;
             long in = strtol(x, &end, 10);
-            print_barcode(year, "00000", in);
-            
-        } else if ( regex_matches(regex_100000_199999, x) ) {
-            printf("100000er match!");
-            char *end;
-            long in = strtol(x, &end, 10);
-            print_barcode(year, "000", in);
-
-        } else if ( regex_matches(regex_range_5000_9999, x) ) {
-            printf("5000er RANGE match!");
-        } else if ( regex_matches(regex_range_100000_199999, x) ) {
-            printf("100000er RANGE match!");
+            print_barcode(year, in);
+            belnr = in;
         }
-        
-        char *end;
-        long in = strtol(x, &end, 10);
-        
-        if ((in >= 5000 && in <= 9999)) {
-            // RANGE: 5000-9999
-            print_barcode(year, "00000", in);
+        else if ( regex_matches(regex_range_5000_9999, x) ) {
+            printf("5000-9999 match!\n");
+            char *end;
+            long from = strtol( substring(x, 0, 4), &end, 10);
+            long to   = strtol( substring(x, 5, 4), &end, 10);
             
-        } else if ((in >= 100000 && in <= 199999)) {
-            // RANGE: 100000-199999
-            print_barcode(year, "000", in);
+            if ( from > to ) {
+                continue;
+            }
             
-        } else if ((in >= 2014 && in <= 2099)) {
+            for ( long i = from; i <= to; i++ ) {
+                print_barcode(year, i);
+            }
+            
+            belnr = to;
+        }
+        else if ( regex_matches(regex_range_100000_199999, x) ) {
+            printf("100000-199999 match!\n");
+            char *end;
+            long from = strtol( substring(x, 0, 6), &end, 10);
+            long to   = strtol( substring(x, 7, 6), &end, 10);
+            
+            if ( from > to ) {
+                continue;
+            }
+            
+            for ( long i = from; i <= to; i++ ) {
+                print_barcode(year, i);
+            }
+            
+            belnr = to;
+        }
+        else if ( regex_matches(regex_year, x) ) {
+            char *end;
+            long in = strtol(x, &end, 10);
             printf("JAHR gesetzt: %ld \n", in);
             year = (int) in;
             write_year_to_file(year);
-            
-        } else {
+        }
+        else if ( regex_matches(regex_plus, x) ) {
+            printf("Inkrement\n");
+            belnr++;
+            print_barcode(year, belnr);
+        }
+        else {
             printf("UNGÜLTIG\n");
         }
-        
     }
     
     /* Free memory allocated to the pattern buffer by regcomp() */
-    regfree(&regex_5000_9999);
-   // regfree(&regex_100000_199999);
+    //regfree(&regex_5000_9999);
+    // regfree(&regex_100000_199999);
     
     return 0;
 }
